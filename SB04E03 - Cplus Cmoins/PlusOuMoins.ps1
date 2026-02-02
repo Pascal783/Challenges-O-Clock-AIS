a3683bdcb7572d83702cfb77bac6b00ef3312adf
Pascal ROBERT <robert_pascal@hotmail.fr>
2026-02-02 17:02:49 +0100

    feat : version 1.0 - jeu basique fonctionnel

--- SB04E03 - Cplus Cmoins/PlusOuMoins.ps1
+++ SB04E03 - Cplus Cmoins/PlusOuMoins.ps1
@@ -1,9 +1,9 @@
 # Configuration pour les accents
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-Write-Host "==========================================" -ForegroundColor Yellow
-Write-Host "       JEU DU NOMBRE MYSTÈRE (1-100)      " -ForegroundColor cyan
-Write-Host "==========================================" -ForegroundColor Yellow
+Write-Host "=========================================="
+Write-Host "       JEU DU NOMBRE MYSTÈRE (1-100)      "
+Write-Host "=========================================="
 Write-Host ""
 
 # 1. Générer un nombre aléatoire entre 1 et 100
@@ -25,25 +25,25 @@ while (-not $trouve) {
         if ($nombrePropose -eq $nombreMystere) {
             # 5. Victoire !
             Write-Host ""
-            Write-Host "Bravo! Tu as trouvé le nombre mystère $nombreMystere !" -ForegroundColor Green 
-            Write-Host "Nombre de tentatives : $tentatives" -ForegroundColor White
+            Write-Host "Bravo! Tu as trouvé le nombre mystère $nombreMystere !"
+            Write-Host "Nombre de tentatives : $tentatives"
             $trouve = $true
         }
         elseif ($nombrePropose -lt $nombreMystere) {
             # 4. Plus grand
-            Write-Host "Nop! c'est plus! Essaie encore!" -ForegroundColor Red
+            Write-Host "Nop! c'est plus! Essaie encore!"
         }
         else {
             # 4. Plus petit
-            Write-Host "Nop! c'est moins! Essaie encore!" -ForegroundColor Blue
+            Write-Host "Nop! c'est moins! Essaie encore!"
         }
     }
     else {
-        Write-Host "Erreur : Met un nombre entre 1 et 100" -ForegroundColor DarkRed
+        Write-Host "Erreur : Met un nombre entre 1 et 100"
     }
 }
 
 # 6. Pause à la fin
 Write-Host ""
-Write-Host "Appuyez sur une touche pour quitter..." -ForegroundColor Gray
+Write-Host "Appuyez sur une touche pour quitter..."
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
