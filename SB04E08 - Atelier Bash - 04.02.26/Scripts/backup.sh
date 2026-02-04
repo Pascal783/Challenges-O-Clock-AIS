160f7f6f448f64c8c9d78e1421ff6b27d18d69b5
Pascal ROBERT <robert_pascal@hotmail.fr>
2026-02-04 11:12:09 +0100

    feat: backup.sh 1.2

--- SB04E08 - Atelier Bash - 04.02.26/Scripts/backup.sh
+++ SB04E08 - Atelier Bash - 04.02.26/Scripts/backup.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-# 1. Vérification d'un dossier en argument
-if [ -z "$1" ]; then
-    echo ""
-    echo "Usage: $0 <chemin du dossier à sauvegarder>"
-    exit 1
-fi
-
-# 2. Variables
+# 1. Variables
 SOURCE=$1
 DEST="/home/administrateur/Documents/backup"
 DATE=$(date +%Y%m%d_%H%M%S)
 NOM_FICHIER="backup_$DATE.tar.gz"
+LOG="/var/log/backup.log"
 
-# 3. Créer le dossier de destination s'il n'existe pas
+# 2. Vérification d'un dossier en argument
+if [ -z "$1" ]; then
+    echo ""
+    echo "Usage: $0 <chemin du dossier à sauvegarder>"
+    echo ""
+    exit 1
+fi
+
+if [ ! -d "$SOURCE" ]; then
+    echo ""
+    echo "Erreur : Le répertoire source '$SOURCE' n'existe pas." | tee -a "$LOG_FILE"
+    echo ""
+    exit 1
+fi
+
+# 3. Vérification espace disque dispo
+DISPO=$(df "$DEST" | awk 'NR==2 {print $4}')
+TAILLE_SOURCE=$(du -s "$SOURCE" | awk '{print $1}')
+
+if [ "$TAILLE_SOURCE" -gt "$DISPO" ]; then
+    echo "ERREUR : Espace disque insuffisant sur $DEST." >> "$LOG"
+    exit 1
+fi
+
+# 4. Créer le dossier de destination s'il n'existe pas
 if [ ! -d "$DEST" ]; then
     echo ""
     echo "Création du dossier $DEST..."
@@ -21,23 +39,25 @@ if [ ! -d "$DEST" ]; then
     mkdir -p "$DEST"
 fi
 
-# 4. Création de l'archive tar.gz
+# 5. Création de l'archive tar.gz
 # -z : compresser (gzip) / -c : créer / -f : fichier
 tar -czf "$DEST/$NOM_FICHIER" "$SOURCE"
 
-# 5. Message de confirmation et taille archive
+# 6. Message de confirmation et taille archive
 if [ $? -eq 0 ]; then
     TAILLE=$(du -h "$DEST/$NOM_FICHIER" | cut -f1)
     echo "-------------------------------------------"
     echo ""
     echo "Sauvegarde créée : $DEST/$NOM_FICHIER"
     echo "Taille de l'archive : $TAILLE"
+    echo "Log : $LOG"
     echo ""
     echo "-------------------------------------------"
 else
     echo "--------------------------------------------"
     echo ""
     echo "Erreur lors de la création de la sauvegarde."
+    echo "Log : $LOG"
     echo ""
     echo "--------------------------------------------"
 fi
